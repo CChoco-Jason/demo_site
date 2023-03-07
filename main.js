9ed1107da9100d40755e15376096f02c7aa0fff6
CChocoJasonYeet <114135161+CChocoJasonYeet@users.noreply.github.com>
2023-03-07 19:37:39 +0800

    3

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 import './style.css'; 
+
+const img = document.getElementById("img"); 
+
+getRandomPerson();
+async function getRandomPerson() {
+    const data  = await fetch("https://randomuser.me/api/"); 
+    const person = await data.json(); 
+    img.src = person.results[0].picture.large; 
+}
+
